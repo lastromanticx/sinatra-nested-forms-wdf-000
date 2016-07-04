0a6507c81a76c02511962a6f017e20b8032fa985
lastromanticx <giladbarkan@live.com>
2016-07-03 23:44:45 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    self.all.clear
+  end
+
+  def initialize params
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    self.class.all << self
+  end
 end
